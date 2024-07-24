45eb4279007fa3102cffbd59cb736ee6943b60fb
harish-dalal <harishdalal76@gmail.com>
2024-07-24 23:35:34 +0530

    init

---
+++ src/main/java/com/example/splitmoney/splitmoney/SplitmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.splitmoney.splitmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitmoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/splitmoney/splitmoney/SplitmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.splitmoney.splitmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitmoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
